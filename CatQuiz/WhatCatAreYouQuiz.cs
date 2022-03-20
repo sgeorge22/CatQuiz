608fc9fa3916a229d9711b21af9d2570f2be6549
sarah george <sgeorge@tql.com>
2022-03-20 11:43:40 -0400

    Add project files.

---
+++ CatQuiz/WhatCatAreYouQuiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatQuiz
+{
+    internal class WhatCatAreYouQuiz
+    {
+        public WhatCatAreYouQuiz()
+        {
+
+        }
+
+        public string WhatCatAreYou()
+        {
+            return string.Empty;
+        }
+    }
+}
